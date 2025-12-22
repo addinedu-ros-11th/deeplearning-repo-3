0d5c15b7a2b4e44e46aaddf85c1df4daabbe3bd6
hyoinYang <hyoinyang02@gmail.com>
2025-12-22 11:45:35 +0900

    fix: edit scroll style, button size

--- kiosk/ui/check_screen.py
+++ kiosk/ui/check_screen.py
@@ -21,15 +21,27 @@ class CheckScreen(QWidget):
             background-color: rgba(255, 109, 31, 0.7);
             color: #222222;
             font-size: 80pt;
-            font-weight: bold;
             padding: 20px;
         """)
         status_bar.setAlignment(Qt.AlignmentFlag.AlignCenter)
         status_bar.setFixedHeight(150)
         layout.addWidget(status_bar)
+        layout.addSpacing(40)
 
-        content_layout = QVBoxLayout()
-        content_layout.setContentsMargins(20, 20, 20, 20)
+        container = QWidget()
+        container.setFixedSize(1018, 1348)
+        container.setStyleSheet("""
+            QWidget {
+                background-color: #F5E7C6;
+                border-top-left-radius: 30px;
+                border-top-right-radius: 30px;
+            }
+        """)
+        container.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
+
+        container_layout = QVBoxLayout(container)
+        container_layout.setContentsMargins(0, 0, 0, 0)
+        container_layout.setSpacing(0)
         
         scroll_area = QScrollArea()
         scroll_area.setWidgetResizable(True)
@@ -38,17 +50,36 @@ class CheckScreen(QWidget):
             QScrollArea {
                 background-color: #F5E7C6;
                 border: none;
+                border-top-left-radius: 30px;
+                border-top-right-radius: 30px;
+                padding-top: 10px;
+                padding-right: 10px;
+
             }
-            QScrollArea > QWidget > QWidget {
+            QScrollBar:vertical {
                 background-color: #F5E7C6;
+                width: 10px;
+                margin: 30px 5px 0px 0px;
+            }
+            QScrollBar::handle:vertical {
+                background-color: rgba(255, 109, 31, 0.5);
+                border-radius: 5px;
+                min-height: 20px;
+            }
+            QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {
+                height: 0px;
             }
         """)
 
-        scroll_content = QWidget()
-        scroll_content.setStyleSheet("background-color: #F5E7C6;")
-        scroll_layout = QVBoxLayout(scroll_content)
-        scroll_layout.setContentsMargins(10, 10, 10, 10)
-        scroll_layout.setSpacing(7)
+        self.scroll_content = QWidget()
+        self.scroll_content.setStyleSheet("""
+                background-color: transparent;
+                border: none;
+                """)
+        self.scroll_layout = QVBoxLayout(self.scroll_content)
+        self.scroll_layout.setContentsMargins(10, 10, 10, 10)
+        self.scroll_layout.setSpacing(7)
+        self.scroll_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         
         for i in range(10):
             item = QLabel(f"아이템 {i+1}")
@@ -60,22 +91,23 @@ class CheckScreen(QWidget):
                 padding: 7px;
             """)
             item.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter)
-            scroll_layout.addWidget(item)
+            self.scroll_layout.addWidget(item)
         
-        scroll_layout.addStretch()
-        scroll_area.setWidget(scroll_content)
-        content_layout.addWidget(scroll_area, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.scroll_layout.addStretch()
+        scroll_area.setWidget(self.scroll_content)
+        container_layout.addWidget(scroll_area, alignment=Qt.AlignmentFlag.AlignCenter)
 
-        layout.addLayout(content_layout)
+        layout.addWidget(container, alignment=Qt.AlignmentFlag.AlignCenter)
         layout.addSpacing(40)
-        
+
         button_layout = QHBoxLayout()
-        button_layout.setSpacing(30)
+        button_layout.setSpacing(100)
+        button_layout.setContentsMargins(20, 0, 20, 20)
         
         call_btn = QPushButton("뒤로")
         call_btn.setStyleSheet("""
             QPushButton {
-                font-size: 50px;
+                font-size: 40px;
                 padding: 15px;
                 background-color: #E6DABD;
                 color: white;
@@ -91,7 +123,7 @@ class CheckScreen(QWidget):
         pay_btn = QPushButton("결제")
         pay_btn.setStyleSheet("""
             QPushButton {
-                font-size: 70px;
+                font-size: 40px;
                 padding: 15px;
                 background-color: #FF6D1F;
                 color: white;
