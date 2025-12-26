83ecacaef9caa179835d4aa981e00e3c510cb04c
rraamm8 <garam.rachel.lee@gmail.com>
2025-12-26 12:32:31 +0900

    fix: remove prototype router

--- central-api/app/api/routes/__init__.py
+++ central-api/app/api/routes/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.routes import menu, tray, review, order, cctv, dashboard, store, device, prototype, inference
+from app.api.routes import menu, tray, review, order, cctv, dashboard, store, device
 
 api_router = APIRouter()
 api_router.include_router(store.router, tags=["store"])
@@ -11,5 +11,5 @@ api_router.include_router(order.router, tags=["order"])
 api_router.include_router(cctv.router, tags=["cctv"])
 api_router.include_router(dashboard.router, tags=["dashboard"])
 
-api_router.include_router(prototype.router, tags=["prototype"])
+# api_router.include_router(prototype.router, tags=["prototype"])
 # api_router.include_router(inference.router, tags=["inference"])
